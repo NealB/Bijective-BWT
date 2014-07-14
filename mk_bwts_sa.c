c6b2750b38eec4e854308d1f8df96932aeb82582
Neal Burns <nnneal@gmail.com>
2014-07-14 16:50:25 -0700

    Implemented speedup

--- mk_bwts_sa.c
+++ mk_bwts_sa.c
@@ -14,8 +14,20 @@
 #	define MARK_TIME(msg) /*nothing*/
 #endif
 
-unsigned char *make_bwts_sa(unsigned char *T, int len);
+void make_bwts_sa(unsigned char *T, int len);
 
+char *text_infilename;
+char *bwts_outfilename;
+
+void make_outfilename(int argc, char **argv)
+{
+	if(argc < 3) {
+		asprintf(&bwts_outfilename, "%s.bwts", text_infilename);
+	}
+	else {
+		bwts_outfilename = argv[2];
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -27,66 +39,22 @@ int main(int argc, char **argv)
 		fprintf(stderr, "If unspecified, output is written to standard output\n");
 		exit(1);
 	}
+	text_infilename = argv[1];
 
 #ifdef SHOW_TIMINGS
 	t = clock();
 #endif
 
-	char *bwts_outfilename = argc < 3 ? NULL : argv[2];
+	make_outfilename(argc, argv);
+
 	map_in(T, len, argv[1]);
 
-	unsigned char *bwts = make_bwts_sa(T, len);
-
-	FILE *bwtsout = bwts_outfilename ? fopen(bwts_outfilename, "w") : stdout;
-	if(!bwtsout) {
-		fprintf(stderr, "Couldn't open BWTS file for writing\n");
-		perror(bwts_outfilename);
-		exit(1);
-	}
-	fwrite(bwts, 1, len, bwtsout);
-
-	MARK_TIME("Write BWTS");
+	make_bwts_sa(T, len);
 
 	return 0;
 }
 
-int duval_2_1(unsigned char *str, int len, int *ans) // Duval 1983, linear-time and O(1)-space.
-{
-	int i, j, k = 0;
-	int factors = 0;
-	while( k < len )
-	{
-		i = k; j = k+1;
-		while( 1 )
-		{
-			if( j < len )
-			{
-				int str_i = str[i], str_j = str[j];
-				if( str_i < str_j )
-				{
-					i = k; j = j+1;
-					continue;
-				}
-				else if( str_i == str_j )
-				{
-					i = i+1;
-					j = j+1;
-					continue;
-				}
-			}
-			do {
-				k += (j-i);
-				ans[ factors++ ] = k;
-			} // e.g., ...ababab...
-			while( k <= i );
-			// k > i
-			break;
-		}//while true
-	}//while k
-	return factors;
-}//duval_2_1(str)
-
-unsigned char *make_bwts_sa(unsigned char *T, int len)
+void make_bwts_sa(unsigned char *T, int len)
 {
 	int min, min_i;
 	int i, j;
@@ -97,138 +65,98 @@ unsigned char *make_bwts_sa(unsigned char *T, int len)
 	MARK_TIME("Suffix sort");
 
 	int *lyndonwords = (int *)malloc(sizeof(int) * len);
-	//for(i=0; i<len; i++) {
-		//isa[sa[i]] = i;
-	//}
-	int factors = duval_2_1(T, len, lyndonwords);
-	factors--;
+
+	int rank, factors=0, last_lw_pos=len;
+	for(rank=0; sa[rank]!=0; rank++) {
+		if(sa[rank] < last_lw_pos) {
+			last_lw_pos = sa[rank];
+			lyndonwords[factors++] = rank;
+		}
+	}
 
 	MARK_TIME("Find Lyndon words");
 
-	min = binary_search_sa(0, T, sa, len);
+	min = rank;
 	min_i = 0;
-	//for(i=0; i<len && min>0; i++) {
-		//if(isa[i] < min) {
-	int n;
-	for(n=0; n<factors; n++) {
-		i = lyndonwords[n];
-		int isa_i = binary_search_sa(i, T, sa, len);
 
-				int endsym = T[i-1];
-				int pred_pass_count = 0;
+	while(factors>0) {
+		int isa_i = lyndonwords[--factors];
+		i = sa[isa_i];
 
-				for(j=isa_i; j<min-1; j++) {
-					pred_pass_count += (endsym == T[sa[j+1]-1]);
-				}
+		int endsym = T[i-1];
+		int pred_pass_count = 0;
 
-			if(i>0) {
-				int lw_start = min_i;
-				int lw_len = i - min_i;
-				int test_rank = min;
+		for(j=isa_i; j<min-1; j++) {
+			pred_pass_count += (endsym == T[sa[j+1]-1]);
+		}
 
-				while(test_rank+1<len && (sa[test_rank+1] > lw_start+lw_len)) {
-					int k = 0;
-					while((sa[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] == T[sa[test_rank+1] + k]) {
-						k++;
-					}
-					if((sa[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] < T[sa[test_rank+1] + k]) {
-						break;
-					}
+		int lw_start = min_i;
+		int lw_len = i - min_i;
+		int test_rank = min;
 
-					pred_pass_count += (endsym == T[sa[test_rank+1]-1]);
-
-					sa[test_rank] = sa[test_rank+1];
-					//isa[sa[test_rank]] = test_rank;
-					test_rank++;
-				}
-				sa[test_rank] = lw_start;
-				//isa[sa[test_rank]] = test_rank;
-
-
-
-				int ref_rank = test_rank;
-				for(j=i; j-->lw_start+1; ) { // iterate through the new lyndon word from end to start
-					int num_to_move_down = pred_pass_count;
-					pred_pass_count = 0;
-					endsym = T[j-1];
-
-
-					//test_rank = isa[j];
-					test_rank = binary_search_sa(j, T, sa, len);
-					int start_rank = test_rank;
-
-					while(test_rank < len-1) {
-						if(!(num_to_move_down--)) {
-							break;
-						}
-#if 0
-						if(j > sa[test_rank+1] || T[j] != T[sa[test_rank+1]] || ref_rank < isa[sa[test_rank+1]+1]) {
-							break;
-						}
-#endif
-						pred_pass_count += (endsym == T[sa[test_rank+1]-1]);
-
-						sa[test_rank] = sa[test_rank+1];
-						//isa[sa[test_rank]] = test_rank;
-						test_rank++;
-					}
-					sa[test_rank] = j;
-					//isa[sa[test_rank]] = test_rank;
-
-					ref_rank = test_rank;
-
-					if(start_rank == test_rank) {
-						break;
-					}
-				}
+		while(test_rank+1<len && (sa[test_rank+1] > lw_start+lw_len)) {
+			int k = 0;
+			while((sa[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] == T[sa[test_rank+1] + k]) {
+				k++;
+			}
+			if((sa[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] < T[sa[test_rank+1] + k]) {
+				break;
 			}
 
-			min = isa_i;
-			min_i = i;
+			pred_pass_count += (endsym == T[sa[test_rank+1]-1]);
+
+			sa[test_rank] = sa[test_rank+1];
+			test_rank++;
+		}
+		sa[test_rank] = lw_start;
+
+
+
+		for(j=i; j-->lw_start+1; ) { // iterate through the new lyndon word from end to start
+			int num_to_move_down = pred_pass_count;
+			int k;
+
+			if(!num_to_move_down) {
+				break;
+			}
+			pred_pass_count = 0;
+			endsym = T[j-1];
+
+			test_rank = binary_search_sa(j, T, sa, len);
+			for(k=0; k<num_to_move_down; k++) {
+				pred_pass_count += (endsym == T[sa[test_rank+1]-1]);
+
+				sa[test_rank] = sa[test_rank+1];
+				test_rank++;
+			}
+			sa[test_rank] = j;
+		}
+
+		min = isa_i;
+		min_i = i;
 	}
-	//free(sa);
 
 	MARK_TIME("Fix sort order");
 
-	unsigned char *bwts = (unsigned char *)malloc(len);
-
-	j = factors;
-	int lw_end_symbol = T[len-1];
-	for(i=0; i<len; i++) {
-		if(sa[i]==0 || (j>0 && (sa[i] == lyndonwords[j-1]))) {
-			bwts[i] = lw_end_symbol;
-			if(sa[i]>0) {
-				lw_end_symbol = T[sa[i]-1];
-			}
-			j--;
-		}
-		else {
-			bwts[i] = T[sa[i]-1];
-		}
+	FILE *bwtsout = (strcmp(bwts_outfilename, "-")==0) ? stdout : fopen(bwts_outfilename, "w");
+	if(!bwtsout) {
+		fprintf(stderr, "Couldn't open BWTS file for writing\n");
+		perror(bwts_outfilename);
+		exit(1);
 	}
 
-#if 0
-	min = len;
-	min_i = 0;
+	last_lw_pos = len;
 	for(i=0; i<len; i++) {
-		if(isa[i] < min) {
-			if(min < len) {
-				bwts[min] = T[i - 1];
-			}
-
-			min = isa[i];
-			min_i = i;
+		int outc;
+		if(sa[i] < last_lw_pos) {
+			outc = T[last_lw_pos-1];
+			last_lw_pos = sa[i];
 		}
 		else {
-			bwts[isa[i]] = T[i - 1];
+			outc = T[sa[i]-1];
 		}
+		putc_unlocked(outc, bwtsout);
 	}
-	bwts[0] = T[len - 1];
-#endif
-	
-	MARK_TIME("Generate BWTS");
 
-	//free(isa);
-
-	return bwts;
+	MARK_TIME("Write BWTS");
 }
