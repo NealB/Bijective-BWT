abec4bbff5dfac20bc3433d30a0b6bfeda48e21b
Neal Burns <neal.burns@achieveinternet.com>
2014-07-16 14:01:37 -0700

    Tinkering

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
 CC = gcc
 #CFLAGS = -O2
-CFLAGS = -O2 -DSHOW_TIMINGS
+CFLAGS = -O3 -DSHOW_TIMINGS -Wall
 #CFLAGS = -ggdb -DSHOW_TIMINGS
 LDFLAGS = -ldivsufsort
 
-mk_bwts: mk_bwts_sa.o map_file.o binsearch_sa.o
-	$(CC) -o $@ $(CFLAGS) mk_bwts_sa.o map_file.o binsearch_sa.o $(LDFLAGS)
+mk_bwts_sa: mk_bwts_sa.o map_file.o binsearch_sa.o
+	$(CC) -o $@ mk_bwts_sa.o map_file.o binsearch_sa.o $(LDFLAGS)
 
-install: mk_bwts
-	cp mk_bwts $(HOME)/bin/
+install: mk_bwts_sa
+	cp mk_bwts_sa $(HOME)/bin/
 
 clean:
-	$(RM) *.o mk_bwts FOO BAR
+	$(RM) *.o mk_bwts_sa FOO BAR
 
 BAR: TEST
 	mk_bwts TEST BAR 
 
-test: mk_bwts TEST BAR
-	./mk_bwts TEST FOO; cmp FOO BAR && echo "Match"
+test: mk_bwts_sa TEST BAR
+	./mk_bwts_sa TEST FOO; cmp FOO BAR && echo "Match"
--- mk_bwts_sa.c
+++ mk_bwts_sa.c
@@ -5,24 +5,29 @@
 #include <stdint.h>
 #include "map_file.h"
 
+#define BWTS_EXTENSION ".bwts"
+
 #ifdef SHOW_TIMINGS
 #	include <time.h>
-	static clock_t t, last_t;
+	static clock_t t, last_t, first_t;
 #	define MARK_TIME(msg) \
 		last_t=t, fprintf(stderr, "%-25s %6.2f\n", msg " time", (double)((t=clock()) - last_t) / (double)CLOCKS_PER_SEC);
 #else
 #	define MARK_TIME(msg) /*nothing*/
 #endif
 
-void make_bwts_sa(unsigned char *T, int len);
+uint32_t binary_search_sa(uint32_t suff, uint8_t *T, uint32_t *sa, uint32_t len);
 
-char *text_infilename;
+void make_bwts_sa(unsigned char *T, int32_t *SA, int len);
+
 char *bwts_outfilename;
 
 void make_outfilename(int argc, char **argv)
 {
 	if(argc < 3) {
-		asprintf(&bwts_outfilename, "%s.bwts", text_infilename);
+		bwts_outfilename = (char *)malloc(strlen(BWTS_EXTENSION) + strlen(argv[1]) + 1);
+		strcpy(bwts_outfilename, argv[1]);
+		strcat(bwts_outfilename, BWTS_EXTENSION);
 	}
 	else {
 		bwts_outfilename = argv[2];
@@ -36,40 +41,43 @@ int main(int argc, char **argv)
 
 	if(argc < 2) {
 		fprintf(stderr, "Usage: mk_bwts_sa <infile> [<outfile.bwts>]\n");
-		fprintf(stderr, "If unspecified, output is written to standard output\n");
+		fprintf(stderr, "If outfile name is -, output is written to standard output\n");
 		exit(1);
 	}
-	text_infilename = argv[1];
 
 #ifdef SHOW_TIMINGS
-	t = clock();
+	first_t = t = clock();
 #endif
 
 	make_outfilename(argc, argv);
 
 	map_in(T, len, argv[1]);
 
-	make_bwts_sa(T, len);
+	int32_t *SA = (int32_t *)malloc(sizeof(int32_t) * len);
+	divsufsort(T, SA, len);
+
+	MARK_TIME("Suffix sort");
+
+	make_bwts_sa(T, SA, len);
+
+#ifdef SHOW_TIMINGS
+	fprintf(stderr, "%-25s %6.2f\n", "Total time", (double)(clock() - first_t) / (double)CLOCKS_PER_SEC);
+#endif
 
 	return 0;
 }
 
-void make_bwts_sa(unsigned char *T, int len)
+void make_bwts_sa(unsigned char *T, int32_t *SA, int len)
 {
 	int min, min_i;
 	int i, j;
 
-	saidx_t *sa = (saidx_t *)malloc(sizeof(saidx_t) * len);
-	divsufsort(T, sa, len);
-
-	MARK_TIME("Suffix sort");
-
 	int *lyndonwords = (int *)malloc(sizeof(int) * len);
 
 	int rank, factors=0, last_lw_pos=len;
-	for(rank=0; sa[rank]!=0; rank++) {
-		if(sa[rank] < last_lw_pos) {
-			last_lw_pos = sa[rank];
+	for(rank=0; SA[rank]!=0; rank++) {
+		if(SA[rank] < last_lw_pos) {
+			last_lw_pos = SA[rank];
 			lyndonwords[factors++] = rank;
 		}
 	}
@@ -81,34 +89,34 @@ void make_bwts_sa(unsigned char *T, int len)
 
 	while(factors>0) {
 		int isa_i = lyndonwords[--factors];
-		i = sa[isa_i];
+		i = SA[isa_i];
 
 		int endsym = T[i-1];
 		int pred_pass_count = 0;
 
 		for(j=isa_i; j<min-1; j++) {
-			pred_pass_count += (endsym == T[sa[j+1]-1]);
+			pred_pass_count += (endsym == T[SA[j+1]-1]);
 		}
 
 		int lw_start = min_i;
 		int lw_len = i - min_i;
 		int test_rank = min;
 
-		while(test_rank+1<len && (sa[test_rank+1] > lw_start+lw_len)) {
+		while(test_rank+1<len && (SA[test_rank+1] > lw_start+lw_len)) {
 			int k = 0;
-			while((sa[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] == T[sa[test_rank+1] + k]) {
+			while((SA[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] == T[SA[test_rank+1] + k]) {
 				k++;
 			}
-			if((sa[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] < T[sa[test_rank+1] + k]) {
+			if((SA[test_rank+1] + k < len) && T[lw_start + (k % lw_len)] < T[SA[test_rank+1] + k]) {
 				break;
 			}
 
-			pred_pass_count += (endsym == T[sa[test_rank+1]-1]);
+			pred_pass_count += (endsym == T[SA[test_rank+1]-1]);
 
-			sa[test_rank] = sa[test_rank+1];
+			SA[test_rank] = SA[test_rank+1];
 			test_rank++;
 		}
-		sa[test_rank] = lw_start;
+		SA[test_rank] = lw_start;
 
 
 
@@ -122,14 +130,14 @@ void make_bwts_sa(unsigned char *T, int len)
 			pred_pass_count = 0;
 			endsym = T[j-1];
 
-			test_rank = binary_search_sa(j, T, sa, len);
+			test_rank = binary_search_sa(j, T, (uint32_t*)SA, len);
 			for(k=0; k<num_to_move_down; k++) {
-				pred_pass_count += (endsym == T[sa[test_rank+1]-1]);
+				pred_pass_count += (endsym == T[SA[test_rank+1]-1]);
 
-				sa[test_rank] = sa[test_rank+1];
+				SA[test_rank] = SA[test_rank+1];
 				test_rank++;
 			}
-			sa[test_rank] = j;
+			SA[test_rank] = j;
 		}
 
 		min = isa_i;
@@ -148,12 +156,12 @@ void make_bwts_sa(unsigned char *T, int len)
 	last_lw_pos = len;
 	for(i=0; i<len; i++) {
 		int outc;
-		if(sa[i] < last_lw_pos) {
+		if(SA[i] < last_lw_pos) {
 			outc = T[last_lw_pos-1];
-			last_lw_pos = sa[i];
+			last_lw_pos = SA[i];
 		}
 		else {
-			outc = T[sa[i]-1];
+			outc = T[SA[i]-1];
 		}
 		putc_unlocked(outc, bwtsout);
 	}
